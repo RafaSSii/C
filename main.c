ed902bb692051741aa58c94936c72b06807d6959
Rafael Santos <149257772+RafaSSii@users.noreply.github.com>
2025-05-19 15:58:22 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (){
+    int opcao[5];
+    printf("Bem-Vindo a biblioteca\n Digite o que deseja:\n 1-Comprar livro\n 2-Registar livro");
+    scanf("%d", opcao);
+    return 0;
+}
